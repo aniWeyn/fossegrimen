3387d132e7c8d14979656465110b6d40a64edd16
Sabina Niewiadomska <sab.niew@gmail.com>
2017-03-12 18:42:44 +0100

    angular button on click color change added

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 (function(){
 'use strict';
 
+angular.module('fossegrimen', [])
+.controller('SpeechInputController', SpeechInputController)
 
+
+function SpeechInputController ($scope, $filter){
+    $scope.isActive = false;
+  
+    $scope.toggleActive = function() {
+    $scope.isActive = !$scope.isActive;
+  };
+}
 })();
